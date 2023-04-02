012fc9969dee4cd8acd8fc9d186de36952233d5d
saravanan m <20110086@hicet.ac.in>
2023-04-02 19:19:32 +0530

    commit

--- todo-app/app.js
+++ todo-app/app.js
@@ -3,17 +3,19 @@ const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 const path = require("path");
+const { Model, Op } = require("sequelize");
+
 
 app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 
 app.get("/", async (request, response) => {
-  const allTodoItems = await Todo.gettodotest();
+  const allTodo = await Todo.gettodotest();
   if (request.accepts("html")) {
-    response.render("index", { allTodoItems });
+    response.render("index", { allTodo });
   } else {
-    response.json(allTodoItems);
+    response.json(allTodo);
   }
 });
 
@@ -61,12 +63,7 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 });
 
 app.delete("/todos/:id", async function (request, response) {
-  console.log("We have to delete a Todo with ID: ", request.params.id);
-  // FILL IN YOUR CODE HERE
 
-  // First, we have to query our database to delete a Todo by ID.
-  // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
-  // response.send(true)
   const todo = await Todo.findByPk(request.params.id);
   try {
     const deleteTodo = await todo.deleteTodo({ todo: request.params.id });
